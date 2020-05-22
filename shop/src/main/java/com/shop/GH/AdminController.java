131af9611323080da637da69244ad495e5f970c2
김건수 <@DESKTOP-OPVH3B7>
2020-05-23 07:08:29 +0900

    commit[bubu];

--- shop/src/main/java/com/shop/GH/AdminController.java
+++ shop/src/main/java/com/shop/GH/AdminController.java
@@ -51,7 +51,11 @@ public class AdminController {
 		return "admin/shopAdd";
 	}
 	
-	
+	//테스트용
+	@RequestMapping(value = "shop.aaan", method = RequestMethod.GET)
+	public String Test() {											
+		return "admin/shopAdd";
+	}
 	
 	
 	
